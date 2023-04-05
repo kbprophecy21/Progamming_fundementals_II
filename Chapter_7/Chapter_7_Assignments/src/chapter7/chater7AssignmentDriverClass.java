67617b3205da211efd664c88aea36171228c9361
Kyle <djprophecy21@gmail.com>
2023-04-05 10:47:27 -0500

    more things on array list

--- Chapter_7/Chapter_7_Assignments/src/chapter7/chater7AssignmentDriverClass.java
+++ Chapter_7/Chapter_7_Assignments/src/chapter7/chater7AssignmentDriverClass.java
@@ -4,31 +4,42 @@ import java.io.FileNotFoundException;
 
 public class chater7AssignmentDriverClass {
 
-    
-
-
     public static void main(String[] args) throws FileNotFoundException {
-        
-        //Test.InputOutputArray();
-        //Test.TestArray();
-        //Test.TestEmployeePayArray();
-        //Test.TestArrayParameters();
 
-        int[] dummy_numbers = {3, 5, 3};
-        //Test.getMaxNumber(numbers);
-        //System.out.println("\n");
-        //Test.getMinNumber(numbers);
-        //double value = Test.average(numbers);
-        //System.out.printf("Average of the list is: %.2f", value);
+        // Test.InputOutputArray();
+        // Test.TestArray();
+        // Test.TestEmployeePayArray();
+        // Test.TestArrayParameters();
 
+        int[] dummy_numbers = { 3, 5, 3 };
+        // Test.getMaxNumber(numbers);
+        // System.out.println("\n");
+        // Test.getMinNumber(numbers);
+        // double value = Test.average(numbers);
+        // System.out.printf("Average of the list is: %.2f", value);
 
-        //Test.TestPartiallyFilledArray();
-        //Test.writeArrayToFile();
+        // Test.TestPartiallyFilledArray();
+        // Test.writeArrayToFile();
 
-        //TestArrays.StringArray();
-        int[] dummylist = {1, 3, 7, 8, 5, 2, 9, 6};
-        //int result = Sequential_Search.LeanerSearch(valuelist, 7);
-        //System.out.println(result);
+        // TestArrays.StringArray();
+        int[] dummylist = { 1, 3, 7, 8, 5, 2, 9, 6 };
+        // int result = Sequential_Search.LeanerSearch(valuelist, 7);
+        // System.out.println(result);
 
     }
 }
+
+/*
+ * ArrayList libary gives you a dynamic style
+ * 
+ * ArrayList <type> (varible name) = new ArrayList<type>();
+ * 
+ * ####REMEMBER TO IMPORT THE CLASS.
+ * 
+ * example: ArrayList<String> nameList = new ArrayList<String>();
+ * 
+ * nameList.add() = "bob";
+ * nameList.add() = "kyle";
+ * 
+ * 
+ */
